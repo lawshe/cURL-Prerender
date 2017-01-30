9069c05fa1a598f5e9dcf593825a663f20d34fc1
Jessica Lawshe <jessica@jlawshe.com>
2017-01-30 16:05:49 -0600

    Added URL length check

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ var recacheUrls = require('./recache_urls.js');
 var curl = require('curlrequest');
 
 var options = {};
-console.log(recacheUrls.length);
-options.url = 'http://api.prerender.io/recache';
-options.headers = config.headers;
-options.data =  config.data;
 
-recacheUrls.forEach(function(url){
-    var cloneOptions = JSON.parse(JSON.stringify(options));
-    cloneOptions.data.url = url;
-    curl.request(cloneOptions, function (err, data) {
-        if (err) {
-            console.error('Error: ', cloneOptions.data.url, err);
-        } else if (data){
-            console.log(data, cloneOptions.data.url);
-        }
+if (recacheUrls && recacheUrls.length > 0) {
+    console.log('Recache ' + recacheUrls.length + ' URLs');
+    options.url = 'http://api.prerender.io/recache';
+    options.headers = config.headers;
+    options.data =  config.data;
+
+    recacheUrls.forEach(function(url){
+        var cloneOptions = JSON.parse(JSON.stringify(options));
+        cloneOptions.data.url = url;
+        curl.request(cloneOptions, function (err, data) {
+            if (err) {
+                console.error('Error: ', cloneOptions.data.url, err);
+            } else if (data){
+                console.log(data, cloneOptions.data.url);
+            }
+        });
     });
-});
+} else {
+    console.log('No URLs to recache. Check recache_urls.js');
+}
